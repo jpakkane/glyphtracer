7be7aebde6021fd891dc4d1682f4c7b0d9febd27
Jussi Pakkanen <jpakkane@kosto>
2010-04-03 00:53:59 +0300

    Easier imports.

--- fonthelper.py
+++ fonthelper.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
 
 import sys
-from PyQt4 import QtCore, QtGui
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
 from editor import Ui_MainWindow
 
-class StartQT4(QtGui.QMainWindow):
+class StartQT4(QMainWindow):
     def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self, parent)
+        QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
  
 if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     myapp = StartQT4()
     myapp.show()
     sys.exit(app.exec_())
