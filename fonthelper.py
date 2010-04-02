82251da96b641002cea10db97caeaf31571ea57f
Jussi Pakkanen <jpakkane@kosto>
2010-04-03 00:36:45 +0300

    Basic app skeleton.

---
+++ fonthelper.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import sys
+from PyQt4 import QtCore, QtGui
+from editor import Ui_MainWindow
+
+class StartQT4(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+ 
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = StartQT4()
+    myapp.show()
+    sys.exit(app.exec_())
