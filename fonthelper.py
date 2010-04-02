84c6f4be483c1349459b4ba568d25c5538a9ddee
Jussi Pakkanen <jpakkane@kosto>
2010-04-03 01:54:13 +0300

    Some image processing.

--- fonthelper.py
+++ fonthelper.py
@@ -6,17 +6,30 @@ from PyQt4.QtGui import *
 
 from editor import Ui_MainWindow
 
+def evaluate_horizontal_cuts(image):
+    (w, h) = (image.width(), image.height())
+    sums = []
+    for j in xrange(h):
+        total = 0
+        for i in xrange(w):
+            total += image.pixelIndex(i, j)
+        sums.append(total)
+    print sums
+    
+
 class Window(QWidget):
     def __init__(self, image_file, parent = None):
         QWidget.__init__(self, parent)
         self.resize(640, 480)
-        self.image = QPixmap(image_file)
+        self.image = QImage(image_file)
+        evaluate_horizontal_cuts(self.image)
 
     def paintEvent(self, event):
         paint = QPainter()
         paint.begin(self)
-        paint.drawPixmap(0, 0, self.image)
+        paint.drawImage(0, 0, self.image)
         paint.end()
+        print self.image.pixelIndex(33, 18)
 
      
 if __name__ == "__main__":
