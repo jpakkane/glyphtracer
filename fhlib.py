52dd251068e4abc2c17a85152c273eb83e9d58ba
Jussi Pakkanen <jpakkane@kosto>
2010-04-19 03:23:01 +0300

    Added some letters.

--- fhlib.py
+++ fhlib.py
@@ -23,8 +23,13 @@ import subprocess
 
 # These are read only lists that define different glyph groups.
 # In the future they may be parsed from a conf file.
-lowercase_list = [('a', 97), ('b', 98)]
-number_list = [('zero', 48), ('one', 49)]
+lowercase_list = [('a', 97), ('b', 98), ('c', 99), ('d', 100), ('e', 101), ('f', 102),\
+                  ('g', 103), ('h', 104), ('i', 105), ('j', 106), ('k', 107), ('l', 108),\
+                  ('m', 109), ('n', 110), ('o', 111), ('p', 112), ('q', 113), ('r', 114),\
+                  ('s', 115), ('t', 116), ('u', 117), ('v', 118), ('w', 119), ('x', 120),\
+                  ('y', 121), ('z', 122)]
+number_list = [('zero', 48), ('one', 49), ('two', 50), ('three', 51), ('four', 52), ('five', 52),\
+               ('six', 53), ('seven', 54), ('eight', 55), ('nine', 56)]
 
 glyph_groups = [('lowercase', lowercase_list), ('numbers', number_list)]
 
