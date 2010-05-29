3cb5c57ad21b719570086e0015c9482926545af2
Jussi Pakkanen <jpakkane@kosto>
2010-05-29 23:01:05 +0300

    Bumped version number.

--- gtlib.py
+++ gtlib.py
@@ -21,7 +21,7 @@
 import os, subprocess, tempfile
 
 program_name = 'Glyphtracer'
-program_version = '1.1'
+program_version = '1.2'
 
 def entry_to_upper(e):
     return (e[0].capitalize(), e[1]-32)
